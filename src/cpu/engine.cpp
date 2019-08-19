7e1fee81fc78d55b3dc1b41e58132c47c7f4a8c1
tony <zhangguof@gmail.com>
2019-08-20 01:48:22 +0800

    split engine.

---
+++ src/cpu/engine.cpp
@@ -0,0 +1,9 @@
+//
+//  engine.cpp
+//  x86emu
+//
+//  Created by tony on 2019/8/19.
+//  Copyright © 2019 tony. All rights reserved.
+//
+
+#include "engine.hpp"
---
+++ src/cpu/engine.hpp
@@ -0,0 +1,14 @@
+//
+//  engine.hpp
+//  x86emu
+//
+//  Created by tony on 2019/8/19.
+//  Copyright © 2019 tony. All rights reserved.
+//
+
+#ifndef engine_hpp
+#define engine_hpp
+
+#include <stdio.h>
+
+#endif /* engine_hpp */
