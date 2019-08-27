4daa7517ed5bf065b511a4a0502fe717c8a45cb3
tony <zhangguof@gmail.com>
2019-08-27 21:37:16 +0800

    add load dll

---
+++ src/loaddll/load_dll.cpp
@@ -0,0 +1,9 @@
+//
+//  load_dll.cpp
+//  x86emu
+//
+//  Created by tony on 2019/8/27.
+//  Copyright © 2019 tony. All rights reserved.
+//
+
+#include "load_dll.hpp"
---
+++ src/loaddll/load_dll.hpp
@@ -0,0 +1,14 @@
+//
+//  load_dll.hpp
+//  x86emu
+//
+//  Created by tony on 2019/8/27.
+//  Copyright © 2019 tony. All rights reserved.
+//
+
+#ifndef load_dll_hpp
+#define load_dll_hpp
+
+#include <stdio.h>
+
+#endif /* load_dll_hpp */
