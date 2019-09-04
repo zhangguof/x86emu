1cb34e1c104f237344867e8d650c3ed954ecd442
tony <zhangguof@gmail.com>
2019-09-04 21:51:49 +0800

    add crt file.

---
+++ src/crt/wrap_crt.cpp
@@ -0,0 +1,9 @@
+//
+//  wrap_crt.cpp
+//  x86emu
+//
+//  Created by tony on 2019/9/4.
+//  Copyright © 2019 tony. All rights reserved.
+//
+
+#include "wrap_crt.hpp"
---
+++ src/crt/wrap_crt.hpp
@@ -0,0 +1,14 @@
+//
+//  wrap_crt.hpp
+//  x86emu
+//
+//  Created by tony on 2019/9/4.
+//  Copyright © 2019 tony. All rights reserved.
+//
+
+#ifndef wrap_crt_hpp
+#define wrap_crt_hpp
+
+#include <stdio.h>
+
+#endif /* wrap_crt_hpp */
