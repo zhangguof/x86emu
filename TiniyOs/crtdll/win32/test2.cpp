e00daccc7cae56f3cb43eab86a5148077c797fc5
tony <zhangguof@gmail.com>
2019-09-23 23:34:14 +0800

    cpp test.

--- TiniyOs/crtdll/win32/test2.cpp
+++ TiniyOs/crtdll/win32/test2.cpp
@@ -1,4 +1,4 @@
-// #include <iostream>
+#include <iostream>
 #include <string>
 
 #define EXPORT __declspec(dllexport)
@@ -21,20 +21,42 @@ WINAPI uint32_t DllMain(void* hinstDLL, uint32_t fdwReason, uint32_t lpvReserved
 	return 1;
 }
 
-int main()
-{
-	std::string h = "hhh";
-	return 0;
-}
+// int main()
+// {
+// 	std::string h = "hhh";
+// 	return 0;
+// }
 
+// #include <unordered_map>
+// std::unordered_map<std::string,int> s_map;
+// class TestObj
+// {
+// 	public:
+// 		TestObj()
+// 		{
+// 			printf("new TestObj!");
+// 		}
+// 	void test(){
+// 		printf("do test!\n");
+// 	}
+// };
+
+// TestObj testobj;
 
 EXPORT int test_cpp()
 {
 	// std::cout<<"hello world!"<<std::endl;
-	// std::string h = "xxhello world";
+	std::string h = "std cout:hello world";
+	std::string name = "ttt";
+	std::cout<<h<<std::endl;
+	// s_map[name] = 1024;
+	// printf("%s:%d\n",name.c_str(),s_map[name]);
+	// std::cout<<h<<std::endl;
 	// printf("%s\n",h.c_str());
 	// puts(h.c_str());
-	printf("hello world\n");
+	// printf("hello world\n");
 	// fputc('X',stdout);
+	// testobj.test();
+
 	return 0;
 }
